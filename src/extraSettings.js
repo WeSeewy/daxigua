cf3576e7193cca45f3226a65139e48f1b86e867c
Yin Wu <wuyin.cs@qq.com>
2021-06-03 11:29:04 +0800

    final

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -38,7 +38,7 @@ let clickChangeFruit = true;
 let adLink = '';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '氧气工厂';
+document.getElementsByTagName("title")[0].innerText = '氧气工坊';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
